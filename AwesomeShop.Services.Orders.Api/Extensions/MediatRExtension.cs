bbc5a88b52e11aea90e8ff691140c1215203fe4e
danieldantasdev <contatolobinhodev@gmail.com>
2023-06-30 21:33:37 -0300

    feat: dependency injection OrderRepository.cs and IOrderRepository.cs

--- AwesomeShop.Services.Orders.Api/Extensions/MediatRExtension.cs
+++ AwesomeShop.Services.Orders.Api/Extensions/MediatRExtension.cs
@@ -1,14 +1,17 @@
 using AwesomeShop.Services.Orders.Application.Commands.Orders;
 using MediatR;
-using Microsoft.Extensions.DependencyInjection;
 
-namespace AwesomeShop.Services.Orders.Application.Extensions;
+namespace AwesomeShop.Services.Orders.Api.Extensions;
 
 public static class MediatRExtension
 {
     public static IServiceCollection AddMediatRExtension(this IServiceCollection serviceCollection)
     {
-        serviceCollection.AddMediatR(typeof(AddOrderCommandInputModel));
+        serviceCollection.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssemblies(typeof(Program).Assembly,
+                typeof(AddOrderCommandInputModel).Assembly);
+        });
         return serviceCollection;
     }
 }
